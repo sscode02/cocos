f703e95affb7b5bfa33b0ee325f9a1f687d0d32c
sscode02 <1227918543@qq.com>
2023-08-12 21:42:02 +0800

    add config

--- packages/backend/main.js
+++ packages/backend/main.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
+app.use(cors({
+  origin:"*"
+}))
 
+let i = 0
 app.post('/login', (req, res) => {
   console.log(req.body)
-  res.send('Hello World!')
+  
+  res.json({a:2})
+})
+
+app.post('/',(req, res)=>{
+  console.log(req.body)
+  res.send('1')
 })
 
 app.listen(port, () => {
